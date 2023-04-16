4b1a748ecdce35f3863291ebdcf1799229c3ec1a
demetre <deme.barbakadze2@gmail.com>
2023-04-16 22:43:12 +0400

    ch7

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,10 +37,16 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # third-party
+    "crispy_forms",
+    "crispy_bootstrap5",
     # local
     'accounts.apps.AccountsConfig',
     'pages.apps.PagesConfig',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5" 
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
@@ -125,7 +131,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [ BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / "staticfiles" 
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
