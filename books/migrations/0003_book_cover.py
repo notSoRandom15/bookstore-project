297087f99515d39a05178f16036ddbdd18ddb6f9
demetre <deme.barbakadze2@gmail.com>
2023-04-19 17:27:52 +0400

    ch14

--- books/migrations/0003_book_cover.py
+++ books/migrations/0003_book_cover.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('books', '0002_review'),
+        ("books", "0002_review"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='book',
-            name='cover',
-            field=models.ImageField(blank=True, upload_to='covers/'),
+            model_name="book",
+            name="cover",
+            field=models.ImageField(blank=True, upload_to="covers/"),
         ),
     ]
